9756c323d39485ebc9d5353d447736c363a6f974
em-wng <97647219+em-wng@users.noreply.github.com>
2025-03-19 00:38:08 -0700

    Update NER_model.py

--- NER_model.py
+++ NER_model.py
@@ -1,9 +1,13 @@
 import pandas as pd
 import spacy
-import argparse
+import os
+import glob
 
 nlp = spacy.load("en_core_web_sm")
-file_path = "Datasets/Science_&_Technology_News/Science_&_Technology_News.csv"
+
+#ref https://stackoverflow.com/questions/77693774/copy-multiple-csv-files-from-multiple-subfolders-in-the-same-parent-directory-t
+input_dir = os.path.join("Datasets", "*", "*.csv")
+#file_path = "Datasets/Science_&_Technology_News/Science_&_Technology_News.csv"
 
 keywords = {
     "companies": ["Pfizer", "Moderna", "Johnson & Johnson", "AstraZeneca", "Merck", "Roche", "GlaxoSmithKline", "UnitedHealth", "Cigna"],
@@ -24,12 +28,17 @@ def entities(text):
 
 if __name__ == "__main__":
 
-    df = pd.read_csv(file_path)
+    csv_files = glob.glob(input_dir)
+    processed_df = []
 
-    df['is_healthcare'] = df["content"].apply(is_healthcare)
+    for file in csv_files:
+        df = pd.read_csv(file)
 
-    healthcare_df = df[df['is_healthcare']].copy()
+        df['is_healthcare'] = df["content"].apply(is_healthcare)
+        healthcare_df = df[df['is_healthcare']].copy()
+        healthcare_df['entities'] = healthcare_df["content"].apply(entities)
 
-    healthcare_df['entities'] = healthcare_df["content"].apply(entities)
+        processed_df.append(healthcare_df)
 
-    healthcare_df.to_csv("healthcare_articles_with_entities.csv", index=False)
+    output_df = pd.concat(processed_df)
+    output_df.to_csv("healthcare_articles_with_entities.csv", index=False)
