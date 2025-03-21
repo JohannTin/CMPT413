69bdd6301f160495466ad31d97bf385f70bcd4c8
em-wng <97647219+em-wng@users.noreply.github.com>
2025-03-20 21:50:14 -0700

    Update NER_model.py

--- NER_model.py
+++ NER_model.py
@@ -9,18 +9,6 @@ nlp = spacy.load("en_core_web_sm")
 input_dir = os.path.join("Datasets", "*", "*.csv")
 #file_path = "Datasets/Science_&_Technology_News/Science_&_Technology_News.csv"
 
-keywords = {
-    "companies": ["Pfizer", "Moderna", "Johnson & Johnson", "AstraZeneca", "Merck", "Roche", "GlaxoSmithKline", "UnitedHealth", "Cigna"],
-    "drugs": ["Lipitor", "Zoloft", "Advil", "Tylenol", "Ibuprofen", "Aspirin"],
-    "vaccines": ["COVID-19 vaccine", "Influenza vaccine", "HPV vaccine", "Pfizer-BioNTech", "Moderna vaccine"]
-}
-
-def is_healthcare(text):
-    if not isinstance(text, str):
-        return False
-    #keywords = ["healthcare", "medical", "medicine"]
-    return any(keyword.lower() in text.lower() for keyword in keywords)
-
 def entities(text):
     doc = nlp(text)
     entity = [(ent.text, ent.label_) for ent in doc.ents]
@@ -33,12 +21,11 @@ if __name__ == "__main__":
 
     for file in csv_files:
         df = pd.read_csv(file)
+        df = df.dropna(subset=['content'])
 
-        df['is_healthcare'] = df["content"].apply(is_healthcare)
-        healthcare_df = df[df['is_healthcare']].copy()
-        healthcare_df['entities'] = healthcare_df["content"].apply(entities)
+        df['entities'] = df["content"].apply(entities)
 
-        processed_df.append(healthcare_df)
+        processed_df.append(df)
 
     output_df = pd.concat(processed_df)
     output_df.to_csv("healthcare_articles_with_entities.csv", index=False)
